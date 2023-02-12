4a316b33796ef58b583de9634a60b8ca3ebba7e3
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-02-12 15:15:56 +0100

    VLoading added (#6)
    * fixed-center utility added
    
    * VLoading component added
    
    * Fix selectors typo

---
+++ src/components/VLoading.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, test, beforeEach } from 'vitest';
+import { mount, type MountingOptions } from '@vue/test-utils';
+import type { VueWrapper } from '@vue/test-utils';
+import VLoading from './VLoading.vue';
+
+describe('VLoading', () => {
+  let wrapper: VueWrapper<InstanceType<typeof VLoading>>;
+  let options: MountingOptions<any, {}>;
+  const mountComponent = () => {
+    wrapper = mount(VLoading, options);
+  };
+
+  beforeEach(() => {
+    options = {
+      shallow: true,
+    };
+
+    mountComponent();
+  });
+
+  const getLabelWrapper = () => wrapper.find('[data-testid="label"]');
+  const getPulseWrapper = () => wrapper.find('[data-testid="pulse"]');
+
+  test('should render the component', () => {
+    expect(getPulseWrapper().exists()).toBe(true);
+    expect(getLabelWrapper().exists()).toBe(true);
+  });
+});
---
+++ src/components/VLoading.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col items-center">
+    <div
+      class="h-16 w-16 rounded-full bg-blue-500 animate-pulse"
+      data-testid="pulse"></div>
+    <p class="mt-2" data-testid="label">Loading</p>
+  </div>
+</template>
