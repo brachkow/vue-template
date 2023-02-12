edc2b39b5ef9563384bc89f8d1ac8ab3d7e8c8c3
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-02-12 04:21:21 +0100

    Clear demo files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-  import TheWelcome from '../components/TheWelcome.vue';
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello World!</h1>
 </template>
