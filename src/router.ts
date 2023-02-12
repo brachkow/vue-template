604f4d8af2541995513fb6dc3210a45eaf4f8bdb
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-02-12 12:54:54 +0100

    Allow auto scroll to hash

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-  ],
+  routes,
+  scrollBehavior(to) {
+    if (to.hash) {
+      return { el: to.hash, top: 16 };
+    }
+  },
 });
 
 export default router;
