4841fca107d619e844e0e5014c6fc1446a69e5d4
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-08-11 17:39:16 +0200

    Set up server

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 1234,
+    host: true,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
