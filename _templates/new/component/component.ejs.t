1b98f893eef594a636f7daefb50850764ce84c54
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-08-23 10:19:11 +0200

    Add i18n

--- _templates/new/component/component.ejs.t
+++ _templates/new/component/component.ejs.t
@@ -3,6 +3,12 @@ to: src/<%= folder %>/<%= name %>/<%= name %>.vue
 ---
 
 <script lang="ts" setup>
+  import { useI18n } from 'vue-i18n';
+
+  const { t } = useI18n({
+    locale: 'en',
+    messages: {},
+  });
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,15 @@ import '@/style.css';
 import App from './App.vue';
 import router from './router';
 
+import { createI18n } from 'vue-i18n';
+
+const i18n = createI18n({
+  legacy: false,
+});
+
 const app = createApp(App);
 
+app.use(i18n);
 app.use(createPinia());
 app.use(router);
 
