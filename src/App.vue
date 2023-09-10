c50da060480b0040dd5e745cbbfae0f24f3eede1
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-09-10 17:28:55 +0200

    Add Toast from vueuse

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
   import { useLoadingStore } from '@/stores/loading';
   import { VLoadingView } from './views/VLoadingView';
+  import Toast from 'primevue/toast';
 
   const loadingStore = useLoadingStore();
 </script>
 
 <template>
+  <Toast />
   <VLoadingView v-if="loadingStore.isLoading" />
   <RouterView v-else />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import router from './router';
 import { createI18n } from 'vue-i18n';
 import PrimeVue from 'primevue/config';
 import Tailwind from 'primevue/passthrough/tailwind';
+import ToastService from 'primevue/toastservice';
 import '@/style.css';
 
 const i18n = createI18n({
@@ -17,5 +18,6 @@ app.use(i18n);
 app.use(createPinia());
 app.use(router);
 app.use(PrimeVue, { unstyled: true, pt: Tailwind }); // Remove pt (and primvue from tailwind's content) if you want to go completely unstyled
+app.use(ToastService);
 
 app.mount('#app');
